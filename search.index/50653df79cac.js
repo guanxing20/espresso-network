862c27a19d2142b94ac422ab8c6c7855ada8ba24
guanxing20 <guanxing20@users.noreply.github.com>
2026-03-23 01:28:56 +0000

    deploy: 9f587d03c8f4fcff8b41bb331a45cbe92c6ffe61

---
+++ search.index/50653df79cac.js
@@ -0,0 +1,1 @@
+rn_("MUYBAGkhaiHIKskq8wABbwGbhwAAAQIAOzAAAAEBACAGAgBLAAAA+gofBhsCoHAAAStrbngbAqBwAAEjjmlsawOwIQHSHYEybnQ=")
--- trait.impl/hotshot_types/traits/block_contents/trait.Transaction.js
+++ trait.impl/hotshot_types/traits/block_contents/trait.Transaction.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["espresso_types",[["impl Transaction for <a class=\"struct\" href=\"espresso_types/v0/v0_1/struct.Transaction.html\" title=\"struct espresso_types::v0::v0_1::Transaction\">Transaction</a>",0]]],["hotshot_example_types",[["impl Transaction for <a class=\"struct\" href=\"hotshot_example_types/block_types/struct.TestTransaction.html\" title=\"struct hotshot_example_types::block_types::TestTransaction\">TestTransaction</a>",0]]]]);
+    const implementors = Object.fromEntries([["espresso_types",[["impl <a class=\"trait\" href=\"hotshot_types/traits/block_contents/trait.Transaction.html\" title=\"trait hotshot_types::traits::block_contents::Transaction\">Transaction</a> for <a class=\"struct\" href=\"espresso_types/v0/v0_1/struct.Transaction.html\" title=\"struct espresso_types::v0::v0_1::Transaction\">Transaction</a>",0]]],["hotshot_example_types",[["impl <a class=\"trait\" href=\"hotshot_types/traits/block_contents/trait.Transaction.html\" title=\"trait hotshot_types::traits::block_contents::Transaction\">Transaction</a> for <a class=\"struct\" href=\"hotshot_example_types/block_types/struct.TestTransaction.html\" title=\"struct hotshot_example_types::block_types::TestTransaction\">TestTransaction</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[194,235]}
+//{"start":59,"fragment_lengths":[352,393]}
