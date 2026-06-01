784127c08c561ffd80ccd6f4b8ab76b5fe3c4add
guanxing20 <guanxing20@users.noreply.github.com>
2026-06-01 02:56:53 +0000

    deploy: 9f587d03c8f4fcff8b41bb331a45cbe92c6ffe61

--- trait.impl/hotshot_task/task/trait.TaskEvent.js
+++ trait.impl/hotshot_task/task/trait.TaskEvent.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["hotshot_task_impls",[["impl&lt;TYPES: <a class=\"trait\" href=\"hotshot_types/traits/node_implementation/trait.NodeType.html\" title=\"trait hotshot_types::traits::node_implementation::NodeType\">NodeType</a>&gt; <a class=\"trait\" href=\"hotshot_task/task/trait.TaskEvent.html\" title=\"trait hotshot_task::task::TaskEvent\">TaskEvent</a> for <a class=\"enum\" href=\"hotshot_task_impls/events/enum.HotShotEvent.html\" title=\"enum hotshot_task_impls::events::HotShotEvent\">HotShotEvent</a>&lt;TYPES&gt;",0]]]]);
+    const implementors = Object.fromEntries([["hotshot_task_impls",[["impl&lt;TYPES: NodeType&gt; TaskEvent for <a class=\"enum\" href=\"hotshot_task_impls/events/enum.HotShotEvent.html\" title=\"enum hotshot_task_impls::events::HotShotEvent\">HotShotEvent</a>&lt;TYPES&gt;",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[513]}
+//{"start":59,"fragment_lengths":[234]}
--- trait.impl/request_response/data_source/trait.DataSource.js
+++ trait.impl/request_response/data_source/trait.DataSource.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["sequencer",[["impl <a class=\"trait\" href=\"request_response/data_source/trait.DataSource.html\" title=\"trait request_response::data_source::DataSource\">DataSource</a>&lt;<a class=\"enum\" href=\"sequencer/request_response/request/enum.Request.html\" title=\"enum sequencer::request_response::request::Request\">Request</a>&gt; for <a class=\"struct\" href=\"sequencer/request_response/data_source/struct.DataSource.html\" title=\"struct sequencer::request_response::data_source::DataSource\">DataSource</a>",0]]]]);
+    const implementors = Object.fromEntries([["sequencer",[["impl DataSource&lt;<a class=\"enum\" href=\"sequencer/request_response/request/enum.Request.html\" title=\"enum sequencer::request_response::request::Request\">Request</a>&gt; for <a class=\"struct\" href=\"sequencer/request_response/data_source/struct.DataSource.html\" title=\"struct sequencer::request_response::data_source::DataSource\">DataSource</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[518]}
+//{"start":59,"fragment_lengths":[377]}
