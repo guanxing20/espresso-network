f02939292b0bc7a38666d342ae2598e23049b174
guanxing20 <guanxing20@users.noreply.github.com>
2026-07-06 02:32:26 +0000

    deploy: 9f587d03c8f4fcff8b41bb331a45cbe92c6ffe61

--- trait.impl/hotshot_types/traits/signature_key/trait.BuilderSignatureKey.js
+++ trait.impl/hotshot_types/traits/signature_key/trait.BuilderSignatureKey.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["espresso_types",[["impl <a class=\"trait\" href=\"hotshot_types/traits/signature_key/trait.BuilderSignatureKey.html\" title=\"trait hotshot_types::traits::signature_key::BuilderSignatureKey\">BuilderSignatureKey</a> for <a class=\"struct\" href=\"espresso_types/v0/v0_1/struct.FeeAccount.html\" title=\"struct espresso_types::v0::v0_1::FeeAccount\">FeeAccount</a>",0]]],["hotshot_types",[]]]);
+    const implementors = Object.fromEntries([["espresso_types",[["impl BuilderSignatureKey for <a class=\"struct\" href=\"espresso_types/v0/v0_1/struct.FeeAccount.html\" title=\"struct espresso_types::v0::v0_1::FeeAccount\">FeeAccount</a>",0]]],["hotshot_types",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[371,21]}
+//{"start":59,"fragment_lengths":[199,21]}
