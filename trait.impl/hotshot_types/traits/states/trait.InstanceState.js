6bdac7612e3c3b30e6037b1d3fbafea1ba41d051
guanxing20 <guanxing20@users.noreply.github.com>
2026-07-27 02:10:38 +0000

    deploy: 9f587d03c8f4fcff8b41bb331a45cbe92c6ffe61

--- trait.impl/hotshot_types/traits/states/trait.InstanceState.js
+++ trait.impl/hotshot_types/traits/states/trait.InstanceState.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["espresso_types",[["impl InstanceState for <a class=\"struct\" href=\"espresso_types/v0/struct.NodeState.html\" title=\"struct espresso_types::v0::NodeState\">NodeState</a>",0]]],["hotshot_example_types",[["impl InstanceState for <a class=\"struct\" href=\"hotshot_example_types/state_types/struct.TestInstanceState.html\" title=\"struct hotshot_example_types::state_types::TestInstanceState\">TestInstanceState</a>",0]]]]);
+    const implementors = Object.fromEntries([["espresso_types",[["impl <a class=\"trait\" href=\"hotshot_types/traits/states/trait.InstanceState.html\" title=\"trait hotshot_types::traits::states::InstanceState\">InstanceState</a> for <a class=\"struct\" href=\"espresso_types/v0/struct.NodeState.html\" title=\"struct espresso_types::v0::NodeState\">NodeState</a>",0]]],["hotshot_example_types",[["impl <a class=\"trait\" href=\"hotshot_types/traits/states/trait.InstanceState.html\" title=\"trait hotshot_types::traits::states::InstanceState\">InstanceState</a> for <a class=\"struct\" href=\"hotshot_example_types/state_types/struct.TestInstanceState.html\" title=\"struct hotshot_example_types::state_types::TestInstanceState\">TestInstanceState</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[179,243]}
+//{"start":59,"fragment_lengths":[325,389]}
