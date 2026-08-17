e7fd93f1f6989704903ae12f807af22a9c8402c0
guanxing20 <guanxing20@users.noreply.github.com>
2026-08-17 00:49:11 +0000

    deploy: 9f587d03c8f4fcff8b41bb331a45cbe92c6ffe61

--- trait.impl/hotshot_types/traits/block_contents/trait.BlockPayload.js
+++ trait.impl/hotshot_types/traits/block_contents/trait.BlockPayload.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["espresso_types",[["impl BlockPayload&lt;<a class=\"struct\" href=\"espresso_types/v0/struct.SeqTypes.html\" title=\"struct espresso_types::v0::SeqTypes\">SeqTypes</a>&gt; for <a class=\"struct\" href=\"espresso_types/v0/v0_1/struct.Payload.html\" title=\"struct espresso_types::v0::v0_1::Payload\">Payload</a>",0]]],["hotshot_example_types",[["impl&lt;TYPES: NodeType&gt; BlockPayload&lt;TYPES&gt; for <a class=\"struct\" href=\"hotshot_example_types/block_types/struct.TestBlockPayload.html\" title=\"struct hotshot_example_types::block_types::TestBlockPayload\">TestBlockPayload</a>",0]]]]);
+    const implementors = Object.fromEntries([["espresso_types",[["impl <a class=\"trait\" href=\"hotshot_types/traits/block_contents/trait.BlockPayload.html\" title=\"trait hotshot_types::traits::block_contents::BlockPayload\">BlockPayload</a>&lt;<a class=\"struct\" href=\"espresso_types/v0/struct.SeqTypes.html\" title=\"struct espresso_types::v0::SeqTypes\">SeqTypes</a>&gt; for <a class=\"struct\" href=\"espresso_types/v0/v0_1/struct.Payload.html\" title=\"struct espresso_types::v0::v0_1::Payload\">Payload</a>",0]]],["hotshot_example_types",[["impl&lt;TYPES: <a class=\"trait\" href=\"hotshot_types/traits/node_implementation/trait.NodeType.html\" title=\"trait hotshot_types::traits::node_implementation::NodeType\">NodeType</a>&gt; <a class=\"trait\" href=\"hotshot_types/traits/block_contents/trait.BlockPayload.html\" title=\"trait hotshot_types::traits::block_contents::BlockPayload\">BlockPayload</a>&lt;TYPES&gt; for <a class=\"struct\" href=\"hotshot_example_types/block_types/struct.TestBlockPayload.html\" title=\"struct hotshot_example_types::block_types::TestBlockPayload\">TestBlockPayload</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[317,275]}
+//{"start":59,"fragment_lengths":[477,597]}
